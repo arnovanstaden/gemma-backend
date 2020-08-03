c9da55693dd0e58a7de8d651c6c4808a89ab8f4d
Arno van Staden <arnovanstaden@gmail.com>
2020-08-03 20:28:25 +0200

    update emails

--- api/routes/application.js
+++ api/routes/application.js
@@ -2,9 +2,19 @@ const express = require("express");
 const router = express.Router();
 const transporter = require("../utils/nodemailer");
 const fs = require("fs");
-const {
-    application
-} = require("express");
+
+
+// -----------------------
+//  ROUTES
+
+
+const search = (nameKey, data) => {
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].name === nameKey) {
+            return data[i];
+        }
+    }
+}
 
 router.post("/", (req, res) => {
     console.log("Therapy application:");
@@ -13,7 +23,8 @@ router.post("/", (req, res) => {
     let message = {
         from: "GEMMA Therapy Application <application@gemmainstitute.com>",
         to: "arnovanstaden@gmail.com",
-        subject: "Therapy Application",
+        subject: `Therapy Application - ${search("name", req.body).value}`,
+        replyTo: search("email", req.body).value,
         html: buildApplicationEmail(req.body)
     };
 
@@ -41,14 +52,6 @@ module.exports = router;
 
 const buildApplicationEmail = (applications) => {
 
-    function search(nameKey) {
-        for (var i = 0; i < applications.length; i++) {
-            if (applications[i].name === nameKey) {
-                return applications[i];
-            }
-        }
-    }
-
     let rawdata = fs.readFileSync("./api/data/questions.json");
     let questions = JSON.parse(rawdata);
 
@@ -57,46 +60,46 @@ const buildApplicationEmail = (applications) => {
         <h3 style="text-decoration: underline;"> Personal Details </h3>
 
         <h4>Name:</h4>
-        <p> ${search("name").value} </p>
+        <p> ${search("name",applications).value} </p>
         </br>
 
         <h4>Date of Birth:</h4>
-        <p> ${search("dob").value} </p>
+        <p> ${search("dob",applications).value} </p>
         </br>
 
         <h4>Email Address:</h4>
-        <p> ${search("email").value} </p>
+        <p> ${search("email",applications).value} </p>
         </br>
 
         <h4>Phone Number:</h4>
-        <p> ${search("phone").value} </p>
+        <p> ${search("phone",applications).value} </p>
         </br>
 
         <h4>Address:</h4>
-        <p> ${search("address").value} </p>
+        <p> ${search("address",applications).value} </p>
         </br>
 
         <h4>Date & Time of Availability:</h4>
-        <p> ${search("availability").value} </p>
+        <p> ${search("availability",applications).value} </p>
         </br>
 
         <br>
         <h3 style="text-decoration: underline;"> 1. Current situation and symptoms </h3>
 
         <h4>1.1 ${questions[1.1]}</h4>
-        <p> ${search("question1-1").value} </p>
+        <p> ${search("question1-1",applications).value} </p>
         </br>
 
         <h4>1.2 ${questions[1.2]}</h4>
-        <p> ${search("question1-2").value} </p>
+        <p> ${search("question1-2",applications).value} </p>
         </br>
 
         <h4>1.3 ${questions[1.3]}</h4>
-        <p> ${search("question1-3").value} </p>
+        <p> ${search("question1-3",applications).value} </p>
         </br>
 
         <h4>1.4 ${questions[1.4]}</h4>
-        <p> ${search("question1-4").value} </p>
+        <p> ${search("question1-4",applications).value} </p>
         </br>
 
   
@@ -104,100 +107,100 @@ const buildApplicationEmail = (applications) => {
         <h3 style="text-decoration: underline;"> 2. Personal history </h3>
 
         <h4>2.1 ${questions[2.1]}</h4>
-        <p> ${search("question2-1").value} </p>
+        <p> ${search("question2-1",applications).value} </p>
         </br>
 
         <h4>2.2 ${questions[2.2]}</h4>
-        <p> ${search("question2-2").value} </p>
+        <p> ${search("question2-2",applications).value} </p>
         </br>
 
         <h4>2.3 ${questions[2.3]}</h4>
-        <p> ${search("question2-3").value} </p>
+        <p> ${search("question2-3",applications).value} </p>
         </br>
 
         <h4>2.4 ${questions[2.4]}</h4>
-        <p> ${search("question2-4").value} </p>
+        <p> ${search("question2-4",applications).value} </p>
         </br>
   
         <br>
         <h3 style="text-decoration: underline;"> 3. Resources </h3>
 
         <h4>3.1 ${questions[3.1]}</h4>
-        <p> ${search("question3-1").value} </p>
+        <p> ${search("question3-1",applications).value} </p>
         </br>
 
         <h4>3.2 ${questions[3.2]}</h4>
-        <p> ${search("question3-2").value} </p>
+        <p> ${search("question3-2",applications).value} </p>
         </br>
 
         <h4>3.3 ${questions[3.3]}</h4>
-        <p> ${search("question3-3").value} </p>
+        <p> ${search("question3-3",applications).value} </p>
         </br>
   
         <br>
         <h3 style="text-decoration: underline;"> 4. Goals/vision </h3>
 
         <h4>4.1 ${questions[4.1]}</h4>
-        <p> ${search("question4-1").value} </p>
+        <p> ${search("question4-1",applications).value} </p>
         </br>
 
         <h4>4.2 ${questions[4.2]}</h4>
-        <p> ${search("question4-2").value} </p>
+        <p> ${search("question4-2",applications).value} </p>
         </br>
 
         <h4>4.3 ${questions[4.3]}</h4>
-        <p> ${search("question4-3").value} </p>
+        <p> ${search("question4-3",applications).value} </p>
         </br>
 
         <h4>4.4 ${questions[4.4]}</h4>
-        <p> ${search("question4-4").value} </p>
+        <p> ${search("question4-4",applications).value} </p>
         </br>
   
         <br>
         <h3 style="text-decoration: underline;"> 6. Medical history </h3>
 
         <h4>Height:</h4>
-        <p> ${search("height").value} cm</p>
+        <p> ${search("height",applications).value} cm</p>
         </br>
 
         <h4>Weight:</h4>
-        <p> ${search("weight").value} kg</p>
+        <p> ${search("weight",applications).value} kg</p>
         </br>
 
         <h4>6.1 Symptoms:</h4>
-        <p> ${search("question6-1").value.length < 1 ? "none" : search("question6-1").value} </p>
+        <p> ${search("question6-1",applications).value.length < 1 ? "none" : search("question6-1",applications).value} </p>
         </br>
 
         <h4>6.2 ${questions[6.2]}</h4>
-        <p> ${search("question6-2").value} </p>
+        <p> ${search("question6-2",applications).value} </p>
         </br>
 
         <h4>6.3 ${questions[6.3]}</h4>
-        <p> ${search("question6-3").value} </p>
+        <p> ${search("question6-3",applications).value} </p>
         </br>
 
         <h4>6.4 ${questions[6.4]}</h4>
-        <p> ${search("question6-4").value} </p>
+        <p> ${search("question6-4",applications).value} </p>
         </br>
 
         <h4>6.5 ${questions[6.5]}</h4>
-        <p> ${search("question6-5").value} </p>
+        <p> ${search("question6-5",applications).value} </p>
         </br>
 
         <h4>6.6 ${questions[6.6]}</h4>
-        <p> ${search("question6-6").value} </p>
+        <p> ${search("question6-6",applications).value} </p>
         </br>
 
         <h4>6.7 ${questions[6.7]}</h4>
-        <p> ${search("question6-7").value} </p>
+        <p> ${search("question6-7",applications).value} </p>
         </br>
 
         <h4>6.8 ${questions[6.8]}</h4>
-        <p> ${search("question6-8").value} </p>
+        <p> ${search("question6-8",applications).value} </p>
         </br>
 
         <h4>6.9 ${questions[6.9]}</h4>
-        <p> ${search("question6-9").value} </p>
+        <p> ${search("question6-9",applications).value} </p>
         </br>
 
         <br>
@@ -205,27 +208,27 @@ const buildApplicationEmail = (applications) => {
       
 
         <h4>6.10.1 ${questions["6.10.1"]}</h4>
-        <p> ${search("question6-10-1").value} </p>
+        <p> ${search("question6-10-1",applications).value} </p>
         </br>
 
         <h4>6.10.2 ${questions["6.10.2"]}</h4>
-        <p> ${search("question6-10-2").value} </p>
+        <p> ${search("question6-10-2",applications).value} </p>
         </br>
 
         <h4>6.10.3 ${questions["6.10.3"]}</h4>
-        <p> ${search("question6-10-3").value} </p>
+        <p> ${search("question6-10-3",applications).value} </p>
         </br>
 
         <h4>6.10.4 ${questions["6.10.4"]}</h4>
-        <p> ${search("question6-10-4").value} </p>
+        <p> ${search("question6-10-4",applications).value} </p>
         </br>
 
         <h4>6.10.5 ${questions["6.10.5"]}</h4>
-        <p> ${search("question6-10-5").value} </p>
+        <p> ${search("question6-10-5",applications).value} </p>
         </br>
 
         <h4>6.10.6 ${questions["6.10.6"]}</h4>
-        <p> ${search("question6-10-6").value} </p>
+        <p> ${search("question6-10-6",applications).value} </p>
         </br>
 
         <br>
@@ -234,30 +237,42 @@ const buildApplicationEmail = (applications) => {
         <h4> Is there a history of illness in your family? (high blood pressure, cardiovascular diseases, cancer, addiction, mental illness, etc.) </h4>
 
         <h4>6.11.1 ${questions["6.11.1"]}</h4>
-        <p> ${search("question6-11-1").value} </p>
+        <p> ${search("question6-11-1",applications).value} </p>
         </br>
 
         <h4>6.11.2 ${questions["6.11.2"]}</h4>
-        <p> ${search("question6-11-2").value} </p>
+        <p> ${search("question6-11-2",applications).value} </p>
         </br>
 
         <h4>6.11.3 ${questions["6.11.3"]}</h4>
-        <p> ${search("question6-11-3").value} </p>
+        <p> ${search("question6-11-3",applications).value} </p>
         </br>
 
         <h4>6.11.4 ${questions["6.11.4"]}</h4>
-        <p> ${search("question6-11-4").value} </p>
+        <p> ${search("question6-11-4",applications).value} </p>
         </br>
 
         <br>
         <h4 style="text-decoration: underline;"> Suicidal Tendencies </h4>
 
         <h4>6.12.1 ${questions["6.12.1"]}</h4>
-        <p> ${search("question6-12-2").value} </p>
+        <p> ${search("question6-12-2",applications).value} </p>
         </br>
 
         <h4>6.12.2 ${questions["6.12.2"]}</h4>
-        <p> ${search("question6-12-2").value} </p>
+        <p> ${search("question6-12-2",applications).value} </p>
+        </br>
+
+        </br>
+        </br>
+        <h3> Terms & Conditions <h3>
+
+        <h4>Costs:</h4>
+        <p> ${search("cost-terms",applications).value} </p>
+        </br>
+
+        <h4>Video Recording</h4>
+        <p> ${search("video-terms",applications).value.join("<br>")} </p>
         </br>
 
     `
